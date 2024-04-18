b62f536e1b0652f067a2c2ccdc0c85baed978888
Raffaelbdl <raffaelbdl@gmail.com>
2024-04-18 14:24:12 +0900

    Add vocabulary list

--- kuma/addon.py
+++ kuma/addon.py
@@ -3,6 +3,7 @@
 from typing import Optional
 
 import aqt
+from aqt.utils import showInfo
 import aqt.qt
 
 from PyQt6 import QtCore
@@ -10,6 +11,7 @@ from PyQt6 import QtCore
 from .anki import KumaAnki
 from .widget import Anki_SearchWidget
 from .widget import JPDB_SearchWidget
+from .widget import JPDB_VocabListWidget
 
 
 class KumaBrowser_Main(aqt.QWidget):
@@ -32,6 +34,12 @@ class KumaBrowser_Main(aqt.QWidget):
         self.jpdb_action.setChecked(False)
         tool_bar.addAction(self.jpdb_action)
 
+        self.jpdb_vl_action = aqt.QAction("JPDB VocabList", self)
+        self.jpdb_vl_action.triggered.connect(self.on_vl_jpdb_triggered)
+        self.jpdb_vl_action.setCheckable(True)
+        self.jpdb_vl_action.setChecked(False)
+        tool_bar.addAction(self.jpdb_vl_action)
+
         self._layout.addWidget(tool_bar)
 
         # Anki Page
@@ -44,19 +52,38 @@ class KumaBrowser_Main(aqt.QWidget):
         self.jpdb_search_widget.hide()
         self._layout.addWidget(self.jpdb_search_widget)
 
+        # JPDB VL Page
+        self.jpdb_vl_widget = JPDB_VocabListWidget(self)
+        self.jpdb_vl_widget.hide()
+        self._layout.addWidget(self.jpdb_vl_widget)
+
         aqt.QShortcut(aqt.QKeySequence("Escape"), self, activated=self.on_Espace)
 
     def on_search_triggered(self) -> None:
         self.jpdb_search_widget.hide()
+        self.jpdb_vl_widget.hide()
         self.anki_search_widget.show()
         self.jpdb_action.setChecked(False)
+        self.jpdb_vl_action.setChecked(False)
         self.search_action.setChecked(True)
 
     def on_jpdb_triggered(self) -> None:
         self.anki_search_widget.hide()
+        self.jpdb_vl_widget.hide()
         self.jpdb_search_widget.show()
         self.search_action.setChecked(False)
+        self.jpdb_vl_action.setChecked(False)
         self.jpdb_action.setChecked(True)
+
+    def on_vl_jpdb_triggered(self) -> None:
+        # showInfo("Not working when not experimenting")
+        # return
+        self.anki_search_widget.hide()
+        self.jpdb_search_widget.hide()
+        self.jpdb_vl_widget.show()
+        self.search_action.setChecked(False)
+        self.jpdb_action.setChecked(False)
+        self.jpdb_vl_action.setChecked(True)
 
     def on_Espace(self):
         self.close()
